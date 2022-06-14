93f19413750adbdcb66a99e7f033aeceb2f256f7
luo-dahui <934803813@qq.com>
2022-06-14 08:01:59 +0000

    ansible支持清除docker容器,镜像

--- config/script/stop_container.sh
+++ config/script/stop_container.sh
@@ -4,24 +4,24 @@ GREEN='\e[1;32m'
 YELLOW='\e[1;33m'
 NC='\e[0m' # 没有颜色
 
-container=""
+image_name=""
 if [ $# -ge 1 ]; then
-    container=$1
+    image_name=$1
 else
-	echo -e "\n\n${YELLOW}please input docker container!!! ${NC}"
+	echo -e "\n\n${YELLOW}please input docker image!!! ${NC}"
 	exit 0
 fi
 
-echo -e "\n\n${YELLOW}Stop docker container:$container ${NC}"
+echo -e "\n\n${YELLOW}Stop docker container, image:$image_name ${NC}"
 
 
 # 停止容器 
-docker ps -a | grep -w $container
+docker ps -a | grep -w $image_name
 if [ $? == 0 ]; then
     echo -e "${YELLOW}======================start to stop container=================${NC}"
-    docker stop $(docker ps -a | grep -w $container | awk '{print $1 }')
+    docker stop $(docker ps -a | grep -w $image_name | awk '{print $1 }')
     # 删除容器
-    # docker rm $(docker ps -a | grep -w $container | awk '{print $1 }')
+    # docker rm $(docker ps -a | grep -w $image_name | awk '{print $1 }')
 fi
 
 echo -e "${GREEN}=======================list docker processes=====================${NC}"
